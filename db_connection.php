f2f086af93aa482a2f3a39a9d350c1e15f01c46e
MOHAMED YASSINE <mohamedyassine_bahajou@um5.ac.ma>
2024-12-30 21:24:39 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,1 @@
-<?php
-// PDO Database connection parameters
-$servername = "localhost"; // Database host
-$username = "root";        // Database username
-$password = "";            // Database password
-$dbname = "feedback_system"; // Database name
 
-// Create PDO connection
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    die();
-}
